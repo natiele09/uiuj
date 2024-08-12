17be1f63fd3352f8b5b9efbbcd83603fc078a4b9
nati <sousanatiele28@gmail.com>
2024-08-12 10:44:20 -0300

    formulario

---
+++ script.js
@@ -0,0 +1,19 @@
+function nome(){
+    const name = document.querySelector("#nom");
+    if(name.value != "natiele"){
+        alert("nome inválido")
+    } 
+    else{
+        alert("ok")
+    }
+}
+/*
+function oi(){
+    const ano = document.querySelector("#idade");
+    if(ano.value >= 18){
+        alert("vc é maior de idade")
+    }
+    else{
+        alert("vc é burro")
+    }
+}/*
